3bcbaf95c126f28dc5f2d9c20f3d018a58c4bec8
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2020-12-13 22:19:25 +0100

    Added comments to day-9 solution

--- src/day-9/index.js
+++ src/day-9/index.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const { readFile } = require('../helpers');
+
+// read file and parse all rows as numbers
 const input = readFile(`${__dirname}\\input.txt`)
   .map(row => parseInt(row));
 
 const { sumItemsInArray } = require('../helpers');
+
+
 
 const getNumberCandidates = (numbers) => {
 
@@ -13,6 +17,9 @@ const getNumberCandidates = (numbers) => {
   }
 
   let candidates = new Set();
+
+  // create a unique array of all possible sums of numbers in the array
+  // this is to be used later to compare if next number is one of the sums in this array (valid number)
 
   for (let i = 0; i < numbers.length - 1; i++) {
     for (let j = i + 1; j < numbers.length; j++) {
@@ -23,19 +30,19 @@ const getNumberCandidates = (numbers) => {
   return Array.from(candidates);
 }
 
-const getSequenceCandidates = (numbers, result) => {
-
-  let numbersToConsider = numbers;
+const getSequenceCandidates = (numbers) => {
 
   let sequences = [];
 
-  for (let i = 0; i < numbersToConsider.length; i++) {
+  // find all possible sequences of at least two numbers
+  // between all the numbers in the list
+  for (let i = 0; i < numbers.length; i++) {
     let group = [];
-    for (let number of numbersToConsider) {
+    for (let number of numbers) {
       group.push(number);
       sequences.push([...group]);
     }
-    numbersToConsider.shift();
+    numbers.shift();
   }
 
   return sequences;
@@ -43,10 +50,19 @@ const getSequenceCandidates = (numbers, result) => {
 
 const findInvalidNumber = (input) => {
 
+  // n is the number of how many previous numbers are considered when creating candidates
   const n = 25;
+
+  // preamble is first n items
   let preamble = input.slice(0, n);
+
+  // all next numbers to be checked if they are valid (all numbers - preamble)
   let numbers = input.slice(n, input.length)
 
+
+  // go through the list and create a list of candidates
+  // check if next number is valid (is included in the candidates)
+  // if number is not valid return it
   for (let number of numbers) {
 
     const candidates = getNumberCandidates(preamble);
@@ -62,7 +78,8 @@ const findInvalidNumber = (input) => {
 
 const findSequence = (input, result) => {
 
-  const sequenceCandidates = getSequenceCandidates(input, result);
+  // find a sequence of numbers whish if summed up is equal to the invalid number we found in part one (result)
+  const sequenceCandidates = getSequenceCandidates(input);
 
   for (let candidate of sequenceCandidates) {
     const arraySum = sumItemsInArray(candidate);
@@ -74,6 +91,7 @@ const findSequence = (input, result) => {
 
 const findEncryptionWeakness = (sequence) => {
 
+  // find min and max number in the sequence, add them together and return the sum
   const sortedSequence = sequence.sort((a, b) => a - b);
   const encryptionWeakness = sortedSequence[0] + sortedSequence[sortedSequence.length - 1];
 
